9fa73d021c6c1e40b5e42b2ddd294407fdf3364d
Raomir Trujillo Peña <raomir1403@gmail.com>
2020-06-02 00:00:54 -0500

    Ajustes de ruta

--- src/app/app-service.service.ts
+++ src/app/app-service.service.ts
@@ -15,8 +15,7 @@ export class AppServiceService {
       'Content-Type': 'application/json',
         'Accept': 'application/json'
     });
-    // this.url = 'https://prueba-fusepong-back.herokuapp.com';
-    this.url = 'http://localhost:3000'
+    this.url = 'https://prueba-fusepong-back.herokuapp.com';
   }
 
   categorias(data): Observable<any> {
